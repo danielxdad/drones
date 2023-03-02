567b26fa8927141ebf0a2c7c011c6e0c1be8ee49
Daniel Lopez <danielxdad@gmail.com>
2023-03-02 14:54:37 -0500

    feat: register models in admin site

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Drone, Medication
 
-# Register your models here.
+
+class InlineMedication(admin.TabularInline):
+    model = Medication
+    extra = 1
+
+
+@admin.register(Drone)
+class DroneAdmin(admin.ModelAdmin):
+    list_display = ['serial_number', 'model', 'weight_limit', 'battery_capacity']
+    search_fields = ['serial_number']
+    list_filter = ['model', 'state']
+    inlines = [InlineMedication]
+
+
+@admin.register(Medication)
+class MedicationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'weight', 'code']
+    search_fields = ['name', 'code']
+    list_filter = ['drone']
