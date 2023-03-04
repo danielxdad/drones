b7e4e3a9ca6903a1a0f4d054623fa381099f7ba8
Daniel Lopez <danielxdad@gmail.com>
2023-03-04 09:26:26 -0500

    feat: setting for drone baterry threshold

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = Path(BASE_DIR).joinpath('media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DRON_BATERRY_THRESHOLD = 25
