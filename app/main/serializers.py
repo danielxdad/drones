512aec6accdfc7abddfb22d611e398fb22843612
Daniel Lopez <danielxdad@gmail.com>
2023-03-04 09:36:33 -0500

    feat: serializers for drone actions

--- app/main/serializers.py
+++ app/main/serializers.py
@@ -1,14 +1,36 @@
 from rest_framework import serializers
+
 from .models import Drone, Medication
 
-
 class DroneSerializer(serializers.ModelSerializer):
+    current_weight = serializers.FloatField(read_only=True)
+
     class Meta:
         model = Drone
-        fields = '__all__'
+        fields = ['id', 'serial_number', 'model', 'weight_limit', 'battery_capacity', 'state', 'current_weight']
+        read_only_fields = ['state', 'current_weight']
 
 
 class MedicationSerializer(serializers.ModelSerializer):
+    drone = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Medication
-        fields = '__all__' 
+        fields = ['id', 'name', 'weight', 'code', 'image', 'drone']
+        read_only_fields = ['drone']
+
+
+class IDMedicationSerializer(serializers.Serializer):
+    medication_item_id = serializers.IntegerField()
+
+
+class DronStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drone
+        fields = ['state']
+
+
+class DronBaterrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drone
+        fields = ['battery_capacity']
