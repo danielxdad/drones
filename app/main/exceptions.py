89da540c70ff1e54ee2d09f5e390fbb68803a70e
Daniel Lopez <danielxdad@gmail.com>
2023-03-04 09:35:16 -0500

    feat: custom exception in the main app

---
+++ app/main/exceptions.py
@@ -0,0 +1,30 @@
+class AppBaseException(Exception):
+    """
+    Base exception for the application.
+    """
+
+    message = ''
+
+    def __init__(self, message = '', *args: object) -> None:
+        super().__init__(message, *args)
+        if message:
+            self.message = message
+    
+    def __str__(self) -> str:
+        return self.message
+
+
+class WeightExceededError(AppBaseException):
+    """
+    Exception if the weight of the medication item exceeds the maximum drone's weight.
+    """
+
+    message = "Drone weight limit exceeded, can't load the item."
+
+
+class DroneInvalidStateError(AppBaseException):
+    message = "The new state is invalid."
+
+
+class DroneBaterryTooLowError(AppBaseException):
+    message = "The dron's battery is too low to fly."
